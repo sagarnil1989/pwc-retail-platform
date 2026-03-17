9859b4b8a317af723357a630d147fa4edaef9d79
sagarnil_dasgupta <sagarnil1989@gmail.com>
2026-03-17 16:44:26 +0100

    auth change

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -23,8 +23,6 @@ terraform {
     storage_account_name = "stterraformstatepwc"
     container_name       = "tfstate"
     key                  = "pwc-retail-dev.tfstate"
-    use_oidc             = true
-    use_azuread_auth     = true
     tenant_id            = "b82fab88-2f13-4992-a425-5bf3069f8df2"
     subscription_id      = "82710df5-bd00-45bb-801d-537e387dface"
     client_id            = "0a25767b-4e60-4b84-9864-c3c541887736"
@@ -34,11 +32,11 @@ terraform {
 provider "azurerm" {
   features {}
 
-  # # optional if you use service principal vars / env vars
-  # subscription_id = var.subscription_id
-  # tenant_id       = var.tenant_id
-  # client_id       = var.client_id
-  # client_secret   = var.client_secret
+  # optional if you use service principal vars / env vars
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
 
 provider "databricks" {
