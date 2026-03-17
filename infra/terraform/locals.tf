3c54b553e65009d375ecdd5f0b7453a4748b745b
sagarnil_dasgupta <sagarnil1989@gmail.com>
2026-03-17 13:31:36 +0100

    file format

--- infra/terraform/locals.tf
+++ infra/terraform/locals.tf
@@ -7,7 +7,7 @@ locals {
     },
     var.tags
   )
-  bronze_notebook_path = "/Workspace/Users/dasgupta.sagarnil@gmail.com/pwc-retail-platform/databricks/bronze/01_bronze_ingestion"
-  silver_pipeline_path = "/Workspace/Users/dasgupta.sagarnil@gmail.com/pwc-retail-platform/databricks/silver/02_silver_lakeflow_pipeline"
-  gold_notebook_path   = "/Workspace/Users/dasgupta.sagarnil@gmail.com/pwc-retail-platform/databricks/gold/03_gold_analytics"
+  bronze_notebook_path = "/Workspace/Users/dasgupta.sagarnil@gmail.com/pwc-retail-platform/databricks/bronze/01_bronze_ingestion.py"
+  silver_pipeline_path = "/Workspace/Users/dasgupta.sagarnil@gmail.com/pwc-retail-platform/databricks/silver/02_silver_lakeflow_pipeline.py"
+  gold_notebook_path   = "/Workspace/Users/dasgupta.sagarnil@gmail.com/pwc-retail-platform/databricks/gold/03_gold_analytics.py"
 }
